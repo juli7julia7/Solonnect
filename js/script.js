489abb3130f2403caa4ca2f6af8f52a0daae5dda
juli7julia7 <juli7julia7@gmail.com>
2024-12-19 18:36:43 +0200

    add scroll off

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ document.addEventListener("click", documentClick);
 function documentClick(e) {
     const targetItem = e.target;
     if(targetItem.closest(".icon-menu")){
-        document.documentElement.classList.toggle('open-menu')
+        document.documentElement.classList.toggle('open-menu');
+        document.querySelector('html').classList.toggle('scroll-off');
     }
 }
