416256e643b5963d4e26214cacabc22110bc34c6
juli7julia7 <juli7julia7@gmail.com>
2025-03-12 16:21:03 +0200

    remove scroll else

--- js/scroll.js
+++ js/scroll.js
@@ -3,8 +3,6 @@ const observer = new IntersectionObserver((entries) => {
     console.log(entry)
     if (entry.isIntersecting) {
       entry.target.classList.add('show');
-    } else {
-      entry.target.classList.remove('show');
     }
   });
 });
@@ -16,46 +14,40 @@ hiddenElements.forEach((el) => observer.observe(el));
 
 
 const observerB = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      console.log(entry)
-      if (entry.isIntersecting) {
-        entry.target.classList.add('showb');
-      } else {
-        entry.target.classList.remove('showb');
-      }
-    });
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('showb');
+    }
   });
-  
-  const hiddenElementsB = document.querySelectorAll('.hiddenb');
-  hiddenElementsB.forEach((el) => observerB.observe(el));
+});
+
+const hiddenElementsB = document.querySelectorAll('.hiddenb');
+hiddenElementsB.forEach((el) => observerB.observe(el));
 
 
-  const observerC = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      console.log(entry)
-      if (entry.isIntersecting) {
-        entry.target.classList.add('showc');
-      } else {
-        entry.target.classList.remove('showc');
-      }
-    });
+const observerC = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('showc');
+    }
   });
-  
-  const hiddenElementsC = document.querySelectorAll('.hiddenc');
-  hiddenElementsC.forEach((el) => observerC.observe(el));
+});
+
+const hiddenElementsC = document.querySelectorAll('.hiddenc');
+hiddenElementsC.forEach((el) => observerC.observe(el));
 
 
 
-  const observerD = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      console.log(entry)
-      if (entry.isIntersecting) {
-        entry.target.classList.add('showd');
-      } else {
-        entry.target.classList.remove('showd');
-      }
-    });
+const observerD = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('showd');
+    }
   });
-  
-  const hiddenElementsD = document.querySelectorAll('.hiddend');
-  hiddenElementsD.forEach((el) => observerD.observe(el));
+});
+
+const hiddenElementsD = document.querySelectorAll('.hiddend');
+hiddenElementsD.forEach((el) => observerD.observe(el));
